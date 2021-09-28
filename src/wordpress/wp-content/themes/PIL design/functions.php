9424b1c9d0e9c00280b01af83ad590560b2cc9b8
Stéphanie Antignac <antignacstephanie17@gmail.com>
2021-09-28 17:18:18 +0200

    header commencement

--- src/wordpress/wp-content/themes/PIL design/functions.php
+++ src/wordpress/wp-content/themes/PIL design/functions.php
@@ -3,6 +3,14 @@
 function register_assets() {
     wp_register_style('style', get_stylesheet_uri('style.css'));
     wp_enqueue_style('style');
+    wp_register_script('LogoAnim', './JS/script.js');
+    wp_enqueue_script('LogoAnim');
 }
 
+//appeller la liaison entre css et php
 add_action('wp_enqueue_scripts', 'register_assets');
+
+register_nav_menus( array(
+	'main' => 'Menu_header',
+	'header' => 'Haut de page',
+));
