7cf23de68161d504cf9f72a23ab26091a030b260
Stéphanie Antignac <antignacstephanie17@gmail.com>
2021-09-28 13:47:47 +0200

    link css/php in functions.php

---
+++ src/wordpress/wp-content/themes/PIL design/functions.php
@@ -0,0 +1,8 @@
+<?
+//lier les pages css et php ensemble
+function register_assets() {
+    wp_register_style('style', get_stylesheet_uri('style.css'));
+    wp_enqueue_style('style');
+}
+
+add_action('wp_enqueue_scripts', 'register_assets');
