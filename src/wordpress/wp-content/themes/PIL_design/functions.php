8cc6ffb3865f367f81fa16fa81b5b03cdd7932b5
Naim Zian <jokeezer@MacBook-Pro-de-Naim.local>
2021-09-29 12:19:37 +0200

    solve sass problems

--- src/wordpress/wp-content/themes/PIL_design/functions.php
+++ src/wordpress/wp-content/themes/PIL_design/functions.php
@@ -1,7 +1,8 @@
 <?
 //lier les pages css et php ensemble
-function register_assets() {
-    wp_register_style('style', get_stylesheet_uri('style.css'));
+function register_assets()
+{
+    wp_register_style('style', get_stylesheet_uri('/css/style.css'));
     wp_enqueue_style('style');
     wp_register_script('LogoAnim', './JS/script.js');
     wp_enqueue_script('LogoAnim');
@@ -10,7 +11,8 @@ function register_assets() {
 //appeller la liaison entre css et php
 add_action('wp_enqueue_scripts', 'register_assets');
 
-function wpc_mime_types($mimes) {
+function wpc_mime_types($mimes)
+{
     $mimes['svg'] = 'image/svg+xml';
     return $mimes;
 }
@@ -20,15 +22,16 @@ add_filter('upload_mimes', 'wpc_mime_types');
 add_action('wp_enqueue_scripts', 'register_assets');
 
 
-register_nav_menus( array(
-	'main' => 'Menu_header',
-	'header' => 'Haut de page',
+register_nav_menus(array(
+    'main' => 'Menu_header',
+    'header' => 'Haut de page',
 ));
 
-function add_file_types_to_uploads($file_types){
+function add_file_types_to_uploads($file_types)
+{
     $new_filetypes = array();
     $new_filetypes['svg'] = 'image/svg+xml';
-    $file_types = array_merge($file_types, $new_filetypes );
+    $file_types = array_merge($file_types, $new_filetypes);
     return $file_types;
-    }
-    add_filter('upload_mimes', 'add_file_types_to_uploads');
+}
+add_filter('upload_mimes', 'add_file_types_to_uploads');
