33a13d894ec4c9176bea1b0933b829ad5701975f
NaimZ <86081641+Naim350Z@users.noreply.github.com>
2021-09-29 10:15:59 +0200

    Ajout SVG

---
+++ src/wordpress/wp-content/themes/PIL design/front-page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pole Image</title>
+
+
+</head>
+
+<body>
+    <img class="fleche-bas" src="" alt="fleche-bas">
+    <h1><?php the_field('punch_line_home'); ?></h1>
+    <img src="" alt="bureaux pole image">
+
+</body>
+
+</html>
