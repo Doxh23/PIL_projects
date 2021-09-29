56e4905dfc77d5ca04cf49bc62ad6b6cb5bf1822
Zoé Ranzy <z.ranzy@hotmail.com>
2021-09-29 16:42:54 +0200

    footer html&scss, not finished

--- src/wordpress/wp-content/themes/PIL_design/footer.php
+++ src/wordpress/wp-content/themes/PIL_design/footer.php
@@ -1,5 +1,22 @@
 <footer>
-    <? wp_footer();?>
+    <div class="footer-left">
+        <h3>Pôle Image<br>de Liège</h3>
+        <p>+32 4 239 69 01</p>
+        <p class="tel">info@lepole.be</p>
+        <p>Rue de Mulhouse, 36</p>
+        <p>4020 Liège, Belgique</p>
+    </div>
+    <div class="footer-right">
+        <ul>
+            <li><a href="">Les entreprises</a></li>
+            <li><a href="template-espaces.php">Nos espaces</a></li>
+            <li><a href="template-pole.php">Le Pôle</a></li>
+            <li><a href="template-contact.php">Contact</a></li>
+        </ul>
+        <small class="small-left">Le Pôle Image © 2021.<br>Politique de confidentialité</small>
+        <small class="small-right">Design by savoirfaire.digital<br>Coding by <a href="becode.org">becode.org</a></small>
+    </div>
+    <?php wp_footer(); ?>
 </footer>
 </body>
 </html>
