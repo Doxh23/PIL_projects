b4ce4d69117a4b17bb4bc7d7c11356a6a61c388f
Naim Zian <jokeezer@MacBook-Pro-de-Naim.local>
2021-09-29 11:29:13 +0200

    SVG + IMG

--- src/wordpress/wp-content/themes/PIL design/front-page.php
+++ src/wordpress/wp-content/themes/PIL design/front-page.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <img class="fleche-bas" src="" alt="fleche-bas">
+    <img class="fleche-bas" src="http://localhost/wordpress/wp-content/uploads/2021/09/arrow-down.svg" alt="fleche-bas">
     <h1><?php the_field('punch_line_home'); ?></h1>
     <img src="" alt="bureaux pole image">
     <? get_header(); ?>
