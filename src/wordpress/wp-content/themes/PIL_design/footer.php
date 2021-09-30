6404a9e7265d3df4a139164334e983deb922a0f6
Zoé Ranzy <z.ranzy@hotmail.com>
2021-09-30 12:23:52 +0200

    update footer html&scss

--- src/wordpress/wp-content/themes/PIL_design/footer.php
+++ src/wordpress/wp-content/themes/PIL_design/footer.php
@@ -1,20 +1,26 @@
 <footer>
-    <div class="footer-left">
-        <h3>Pôle Image<br>de Liège</h3>
-        <p>+32 4 239 69 01</p>
-        <p class="tel">info@lepole.be</p>
-        <p>Rue de Mulhouse, 36</p>
-        <p>4020 Liège, Belgique</p>
-    </div>
-    <div class="footer-right">
-        <ul>
-            <li><a href="">Les entreprises</a></li>
-            <li><a href="template-espaces.php">Nos espaces</a></li>
-            <li><a href="template-pole.php">Le Pôle</a></li>
-            <li><a href="template-contact.php">Contact</a></li>
-        </ul>
-        <small class="small-left">Le Pôle Image © 2021.<br>Politique de confidentialité</small>
-        <small class="small-right">Design by savoirfaire.digital<br>Coding by <a href="becode.org">becode.org</a></small>
+    <hr class="border">
+    <div class="foot">
+        <div class="footer-left">
+            <h3>Pôle Image<br>de Liège</h3>
+            <a class="tel" href="tel:+3242396901">+32 4 239 69 01</a>
+            <p><a href="mailto:info@lepole.be">info@lepole.be</a><br>
+            Rue de Mulhouse, 36<br>
+            4020 Liège, Belgique</p>
+        </div>
+        <hr>
+        <div class="footer-right">
+            <ul>
+                <li><a href="">Les entreprises</a></li>
+                <li><a href="">Nos espaces</a></li>
+                <li><a href="">Le Pôle</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+            <div class="smalls">
+                <small class="small-left">Le Pôle Image © 2021.<br><a href="politique-de-confidentialite">Politique de confidentialité</a></small>
+                <small class="small-right">Design by <a href="https://savoirfaire.digital/en/">savoirfaire.digital</a><br>Coding by <a href="https://becode.org/">becode.org</a></small>
+            </div>
+        </div>
     </div>
     <?php wp_footer(); ?>
 </footer>
