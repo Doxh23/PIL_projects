ea20a49c17d5b74063bb1fe68e54054b112a450d
Naim Zian <jokeezer@MacBook-Pro-de-Naim.local>
2021-09-30 09:06:25 +0200

    30septUPDATE

--- src/wordpress/wp-content/themes/PIL_design/front-page.php
+++ src/wordpress/wp-content/themes/PIL_design/front-page.php
@@ -1,8 +1,8 @@
 <? get_header(); ?>
-
-
-<img class="fleche-bas" src="http://localhost/wordpress/wp-content/uploads/2021/09/arrow-down.svg" alt="fleche-bas">
-<h1><?php the_field('punch_line_home'); ?></h1>
-<img src="http://localhost/wordpress/wp-content/uploads/2021/09/Mask-Group-79@2x.jpg" alt="bureaux pole image">
-
+<div class="sectionOne">
+    <img class="fleche-bas" src="http://localhost/wordpress/wp-content/uploads/2021/09/arrow-down.svg" alt="fleche-bas">
+    <h1><?php the_field('punch_line_home'); ?></h1>
+    <img class="bureaux-PI" src="http://localhost/wordpress/wp-content/uploads/2021/09/Mask-Group-79@2x.jpg" alt="bureaux pole image">
+    <?php the_field('cta-test') ?>
+</div>
 <? get_footer(); ?>
