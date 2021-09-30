973a0775fa9d6c0c481cbc60a15b9d74095c17c8
Adrien Péters <adrien.peters95@gmail.com>
2021-09-30 16:37:27 +0200

    img acf home page

--- src/wordpress/wp-content/themes/PIL_design/front-page.php
+++ src/wordpress/wp-content/themes/PIL_design/front-page.php
@@ -5,10 +5,11 @@
     <div class="image-fleche">
 
         <img class="fleche-bas" src="http://localhost/wordpress/wp-content/uploads/2021/09/arrow-down.svg" alt="fleche-bas">
-        <a href="<?php $image = get_field('image_home');
-                    var_dump($image['sizes']); ?>"></a>
+        
     </div>
 
     <?php the_field('introduction') ?>
 </div>
+<img src="<?php $image = get_field('image_home');
+        echo $image['sizes']['large'];  ?>">
 <? get_footer(); ?>
