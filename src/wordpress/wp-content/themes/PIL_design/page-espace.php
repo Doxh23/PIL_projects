9050cc26919c6a11f13d3dcb6c8e2cffa446b000
Adrien Péters <adrien.peters95@gmail.com>
2021-09-30 11:27:02 +0200

    debut page espace

---
+++ src/wordpress/wp-content/themes/PIL_design/page-espace.php
@@ -0,0 +1,41 @@
+<? get_header(); ?>
+
+
+<div class="espace-titre"><h1>
+    espace et outils de production
+</h1></div>
+<div class="espace-mask-group"><img src="http://localhost/wordpress/wp-content/uploads/2021/09/Mask-Group-79@2x.jpg" alt=""></div>
+<div>
+    <div class="espace-titre-auxiliaire">
+<h2>Nos Espaces</h2>
+<p>Des espaces polyvalents de 15 à 1000m²</p>
+</div>
+<div class="espace-reserver">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nam impedit sed accusamus modi ea.</p>
+    <a href="">reservez vos places</a>
+</div>
+
+</div>
+
+
+<div>
+<div>
+<h2>Nos outils de production</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex quam voluptatibus possimus ducimus numquam aliquid temporibus rerum ab qui a.</p>
+</div>
+<div class="test"><img src="http://localhost/wordpress/wp-content/uploads/2021/09/Image-63@2x.jpg" alt=""></div>
+
+
+</div>
+<div>
+
+<div class="accordeons"></div>
+<div class="accordeons"></div>
+<div class="accordeons"></div>
+<div class="accordeons"></div>
+<div class="accordeons"></div>
+<div class="accordeons"></div>
+</div>
+<div></div>
+
+<? get_footer(); ?>
