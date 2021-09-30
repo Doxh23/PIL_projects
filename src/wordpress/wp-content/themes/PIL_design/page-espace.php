d036857aeb61c998548a898c331d51e8e29eb6bf
Adrien Péters <adrien.peters95@gmail.com>
2021-09-30 16:22:05 +0200

    avancement espace

--- src/wordpress/wp-content/themes/PIL_design/page-espace.php
+++ src/wordpress/wp-content/themes/PIL_design/page-espace.php
@@ -2,24 +2,24 @@
 
 
 <div class="espace-titre"><h1>
-    espace et outils de production
+    espace et outils <br>de production
 </h1></div>
 <div class="espace-mask-group"><img src="http://localhost/wordpress/wp-content/uploads/2021/09/Mask-Group-79@2x.jpg" alt=""></div>
-<div>
+<div class="espace-rubrique">
     <div class="espace-titre-auxiliaire">
 <h2>Nos Espaces</h2>
 <p>Des espaces polyvalents de 15 à 1000m²</p>
 </div>
 <div class="espace-reserver">
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos nam impedit sed accusamus modi ea.</p>
-    <a href="">reservez vos places</a>
+    <a class="img-espace-reserver" href="">reservez vos places  <img src="http://localhost/wordpress/wp-content/uploads/2021/09/add.svg" alt=""></a></div>
+
+
 </div>
 
-</div>
 
-
-<div>
-<div>
+<div class="espace-production">
+<div class="nos-productions">
 <h2>Nos outils de production</h2>
 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex quam voluptatibus possimus ducimus numquam aliquid temporibus rerum ab qui a.</p>
 </div>
