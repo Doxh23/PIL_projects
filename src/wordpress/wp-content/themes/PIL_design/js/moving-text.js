572992a519aeec450df6c0fe2b0ba8e492586d0a
Naim Zian <jokeezer@MacBook-Pro-de-Naim.local>
2021-10-04 16:17:56 +0200

    section moving text2

--- src/wordpress/wp-content/themes/PIL_design/js/moving-text.js
+++ src/wordpress/wp-content/themes/PIL_design/js/moving-text.js
@@ -1,7 +1,6 @@
 
-let text1 = document.getElementById('moving-text');
+let text1 = document.querySelector('moving-text');
 
 document.addEventListener('scroll', function(){
   text1.style.left = Math.max( 50, ( window.innerWidth / 10000 ) * window.scrollY ) + '%';
 });
-
