b2bce346f01df3cd0d268e154682cca2fd23c6ab
Zoé Ranzy <z.ranzy@hotmail.com>
2021-10-05 16:31:33 +0200

    still doesn't display my code, but hey

--- src/wordpress/wp-content/themes/PIL_design/page-contact.php
+++ src/wordpress/wp-content/themes/PIL_design/page-contact.php
@@ -7,11 +7,14 @@
 
 <main class="contact">
     <div class="contact-sidebar">
-        <p>adresse</p>
+        <h3>Contact</h3>
+        <p>Rue de Mulhouse, 36<br>4020 Liège, Belgique</p>
+        <input type="button" value="Itinéraire" id="itinerary">
     </div>
     <div class="contact-right">
         <div class="contact-mail">
-            <h1>mail</h1>
+            <p class="big-txt"><a href="mailto:info@lepole.be">info@lepole.be</a><br>
+            <a href="tel:+3242396901">+32 4 239 69 01</a></p>
         </div>
         <div class="map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.681321978811!2d5.584626415427773!3d50.63303657950093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0f0abbfc177c7%3A0xf2c9f9204e1bb92b!2zUMO0bGUgaW1hZ2UgZGUgTGnDqGdl!5e0!3m2!1sfr!2sbe!4v1633358467386!5m2!1sfr!2sbe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
