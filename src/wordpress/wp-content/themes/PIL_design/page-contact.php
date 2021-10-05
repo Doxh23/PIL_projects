d4bcb9387373f54e5fceab3996d6e6ddfc5c67a1
Zoé Ranzy <z.ranzy@hotmail.com>
2021-10-05 14:09:03 +0200

    base structure progress

--- src/wordpress/wp-content/themes/PIL_design/page-contact.php
+++ src/wordpress/wp-content/themes/PIL_design/page-contact.php
@@ -1,11 +1,22 @@
 <? get_header(); ?>
+<? 
+/*
+* Template Name: "Contact"
+*/
+?>
+
 <main class="contact">
     <div class="contact-sidebar">
+        <p>adresse</p>
     </div>
     <div class="contact-right">
+        <div class="contact-mail">
+            <h1>mail</h1>
+        </div>
         <div class="map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.681321978811!2d5.584626415427773!3d50.63303657950093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0f0abbfc177c7%3A0xf2c9f9204e1bb92b!2zUMO0bGUgaW1hZ2UgZGUgTGnDqGdl!5e0!3m2!1sfr!2sbe!4v1633358467386!5m2!1sfr!2sbe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </div>
 </main>
+
 <? get_footer(); ?>
