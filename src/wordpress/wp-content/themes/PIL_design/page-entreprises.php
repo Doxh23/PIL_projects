9af4c0931b32a7f39f7ac16028d789a97a4d1c12
Stéphanie Antignac <antignacstephanie17@gmail.com>
2021-10-06 13:46:56 +0200

    post archive entreprise

--- src/wordpress/wp-content/themes/PIL_design/page-entreprises.php
+++ src/wordpress/wp-content/themes/PIL_design/page-entreprises.php
@@ -11,35 +11,31 @@
         </div>
         <div class="droite">
         <? get_search_form(); ?>
-                <? if(have_rows('entreprises')) : while(have_rows('entreprises')) : the_row();
-                $titre_ent = get_sub_field('titre_entreprise');
-                $image_ent = get_sub_field('image_entreprise');
-                $email_ent = get_sub_field('email_entreprise');
-                $telephone_ent = get_sub_field('telephone_entreprise');
-                $site_web_ent = get_sub_field('site_web_entreprise');
-                $description_ent = get_sub_field('description_entreprise');
-                ?>
+                <?php if( have_posts() ) : while( have_posts() ) : the_post(); 
+                //var_dump($post);
+                the_title();?>
+
                         <div class="secteur__entreprise">
                                 <button class="secteur__accordion">
                                         <div class="secteur__titre">
-                                                <h2><?= $titre_ent?></h2>
-                                                <?php foreach(get_sub_field('taxonomie_entreprise') as $data){ ?>
+                                                <h2><? the_field('nom__entreprise')?></h2>
+                                                <?php foreach(get_field('taxonomie_entreprise') as $data){ ?>
                                                         <h6> <?php echo $data;?></h6>
                                                 <? }?>
                                         </div>
                                         <img class="secteur__btn" src="http://localhost/wordpress/wp-content/uploads/2021/09/arrow-down.svg" alt="button flèche vers le bas">
                                 </button>            
                                 <div class="secteur__panel">
-                                        <img class="secteur__img" src="<?= $image_ent?>" alt="<? echo $titre_ent?>">
+                                        <img class="secteur__img" src="<? the_field('image_entreprise')?>" alt="<? the_field('nom__entreprise')?>">
                                         <div class="secteur__info">
-                                                <h6><?= $telephone_ent?></h6>
-                                                <h6><?= $email_ent?></h6>
+                                                <h6><? the_field('telephone_entreprise')?></h6>
+                                                <h6><? the_field('email_entreprise')?></h6>
                                                 <h6>
-                                                        <a href="<?= $site_web_ent?>">
-                                                        <?= $site_web_ent?>
+                                                        <a href="<? the_field('site_web_entreprise')?>">
+                                                        <? the_field('site_web_entreprise')?>
                                                         </a>
                                                 </h6>
-                                                <p><?= $description_ent?></p>
+                                                <p><? the_field('description_entreprise')?></p>
                                         </div>
                                 </div>
                         </div>
