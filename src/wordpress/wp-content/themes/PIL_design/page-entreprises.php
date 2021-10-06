f304b34085dd19f422a35b0ad90d5957341ca945
Naim Zian <jokeezer@MacBook-Pro-de-Naim.local>
2021-10-06 17:00:31 +0200

    EOTD

--- src/wordpress/wp-content/themes/PIL_design/page-entreprises.php
+++ src/wordpress/wp-content/themes/PIL_design/page-entreprises.php
@@ -4,6 +4,38 @@
 */
 ?>
 <? get_header(); ?>
+
+<?php 
+// 1. On définit les arguments pour définir ce que l'on souhaite récupérer
+$args = array(
+    'post_type' => 'post',
+    'category_name' => 'films',
+    'posts_per_page' => 3,
+);
+
+// 2. On exécute la WP Query
+$my_query = new WP_Query( $args );
+
+// 3. On lance la boucle !
+if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+    
+    the_title();
+    the_content();
+    the_post_thumbnail();
+
+endwhile;
+endif;
+
+// 4. On réinitialise à la requête principale (important)
+wp_reset_postdata();
+
+?>
+
+
+
+
+
+
 <section class="secteurs">
         <div class="gauche">
                 <h1><? the_title();?></h1>
