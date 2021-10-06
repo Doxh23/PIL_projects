63428ad88b3d6f893994deaebc776658b1a826f3
Zoé Ranzy <z.ranzy@hotmail.com>
2021-10-06 12:28:09 +0200

    lunch commit, starting to work

--- src/wordpress/wp-content/themes/PIL_design/page-contact.php
+++ src/wordpress/wp-content/themes/PIL_design/page-contact.php
@@ -5,21 +5,21 @@
 */
 ?>
 
-<main class="contact">
-    <div class="contact-sidebar">
+<div class="contact">
+    <div class="contact-left">
         <h3>Contact</h3>
-        <p>Rue de Mulhouse, 36<br>4020 Liège, Belgique</p>
-        <input type="button" value="Itinéraire" id="itinerary">
+        <div class="iti"><p>Rue de Mulhouse, 36<br>4020 Liège, Belgique</p>
+        <input type="button" value="Itinéraire" id="itinerary"></div>
     </div>
     <div class="contact-right">
-        <div class="contact-mail">
-            <p class="big-txt"><a href="mailto:info@lepole.be">info@lepole.be</a><br>
-            <a href="tel:+3242396901">+32 4 239 69 01</a></p>
+        <div class="contact-contact">
+            <p><a href="mailto:info@lepole.be">info@lepole.be</a><br>
+            <div class="tel"><div class="plus">+</div><a href="tel:+3242396901">32 4 239 69 01</a></div></p>
         </div>
         <div class="map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.681321978811!2d5.584626415427773!3d50.63303657950093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c0f0abbfc177c7%3A0xf2c9f9204e1bb92b!2zUMO0bGUgaW1hZ2UgZGUgTGnDqGdl!5e0!3m2!1sfr!2sbe!4v1633358467386!5m2!1sfr!2sbe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </div>
-</main>
+</div>
 
 <? get_footer(); ?>
