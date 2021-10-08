73b4a6677541b05d710fc792e77e86f868cd2659
Adrien Péters <adrien.peters95@gmail.com>
2021-10-08 11:27:18 +0200

    xjisjicsq

--- src/wordpress/wp-content/themes/PIL_design/js/moving-text.js
+++
@@ -1,8 +0,0 @@
-let text1 = document.getElementById('moving-text')  ;
-if (text1 === null){
-
-}
-else{
-document.addEventListener('scroll', function(){
-  text1.style.left = Math.max( 50, ( window.innerWidth / 10000 ) * window.scrollY ) + '%';
-});}
--- src/wordpress/wp-content/themes/PIL_design/js/script.js
+++
@@ -1,7 +0,0 @@
-let animation = bodymovin.loadAnimation({
-    container: document.getElementById('animContainer'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'src/wordpress/wp-content/themes/PIL design/Json files/animation.json' // lottie file path
-  })
